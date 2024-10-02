e8935e2a337dd8c7c5d33f26020ca135639e0a66
Mikhaella Nadonga <soriaomikhaella@gmail.com>
2024-10-02 10:49:44 +0800

    Add files via upload

---
+++ taskCalendar.js
@@ -0,0 +1,13 @@
+//create a program that prompts the user to enter the days of the week.
+//use switch statement to map each day to a specific task.
+//include default case for any unexpected output.
+
+let day = prompt("enter your day of the week:");
+day1 = 'sunday';
+day2 = 'monday';
+
+let tasks1 = prompt("your tasks is to rest.");
+let task2 = prompt("your task is to study.");
+
+console.log(day1+task1);
+console.log(day2+task2);
